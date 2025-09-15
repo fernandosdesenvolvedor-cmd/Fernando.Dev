9844b3e3f10ca1d110ce18d1cd85dfcf64bdd9e4
fernandosdesenvolvedor-cmd <fernando.s.desenvolvedor@gmail.com>
2025-09-15 18:03:41 -0300

    Projeto Web - Starbucks

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+
+let circulo = document.querySelector(".circulo")
+let imagemCopo = document.querySelector(".imagem-copo")
+
+
+function mudarCor(cor) {
+ 
+    circulo.style.backgroundColor = cor
+
+}
+
+function trocaimagem(imagem){
+    imagemCopo.src = imagem
+
+}
+
+   
